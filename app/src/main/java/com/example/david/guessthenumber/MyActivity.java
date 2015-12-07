a59f7d05f9b2a9eb7b015d4746f3d6652c77ccc0
DenesDavid <denesd93@gmail.com>
2015-12-08 00:03:25 +0100

    Functional development

--- app/src/main/java/com/example/david/guessthenumber/MyActivity.java
+++ app/src/main/java/com/example/david/guessthenumber/MyActivity.java
@@ -2,10 +2,12 @@ package com.example.david.guessthenumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -33,18 +35,23 @@ public class MyActivity extends AppCompatActivity {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int guess = Integer.parseInt(number.getText().toString());
-                if (guess == TheNumber)
+                if(TextUtils.isEmpty(number.getText().toString()))
                 {
-                    ok.setEnabled(false);
-                    result.setText("Congrats!");
+                    Toast.makeText(getApplicationContext(), "Type number please!!",
+                            Toast.LENGTH_LONG).show();
                 }
                 else {
-                    if (guess < TheNumber) {
-                        result.setText("The number is GREATER than your tip!");
-                    }
-                    if (guess > TheNumber) {
-                        result.setText("The number is SMALLER than your tip!");
+                    int guess = Integer.parseInt(number.getText().toString());
+                    if (guess == TheNumber) {
+                        ok.setEnabled(false);
+                        result.setText("Congrats!");
+                    } else {
+                        if (guess < TheNumber) {
+                            result.setText("The number is GREATER than your tip!");
+                        }
+                        if (guess > TheNumber) {
+                            result.setText("The number is SMALLER than your tip!");
+                        }
                     }
                 }
             }
@@ -57,13 +64,24 @@ public class MyActivity extends AppCompatActivity {
                 result.setText("");
                 TheNumber = GenerateNumber();
                 ok.setEnabled(true);
+                Toast.makeText(getApplicationContext(), "New game started!",
+                        Toast.LENGTH_LONG).show();
             }
         });
         set.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                area=Integer.parseInt(txtarea.getText().toString());
-                ok.setEnabled(false);
+                if(TextUtils.isEmpty(txtarea.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Type number please!!",
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    area = Integer.parseInt(txtarea.getText().toString());
+                    ok.setEnabled(false);
+                    Toast.makeText(getApplicationContext(), "Please start a new game! Area set: 1 - " + area,
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
         exit.setOnClickListener(new View.OnClickListener() {
