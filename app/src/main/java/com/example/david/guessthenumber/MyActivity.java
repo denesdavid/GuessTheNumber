6114f18192828cff2c7633ebae6ebab85bdb535f
DenesDavid <denesd93@gmail.com>
2015-12-07 16:33:58 +0100

    GUI designed

--- app/src/main/java/com/example/david/guessthenumber/MyActivity.java
+++ app/src/main/java/com/example/david/guessthenumber/MyActivity.java
@@ -2,12 +2,25 @@ package com.example.david.guessthenumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MyActivity extends AppCompatActivity {
+
+    private EditText number;
+    private Button newgame,ok;
+    private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        number=(EditText)findViewById(R.id.txtNumber);
+        newgame=(Button)findViewById(R.id.BTNewGame);
+        ok=(Button)findViewById(R.id.BTOK);
+        result=(TextView)findViewById(R.id.txtResult);
     }
 }
