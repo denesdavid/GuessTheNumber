a0031eaf522bc9bbb813cbab1ce5a0657a94888b
DenesDavid <denesd93@gmail.com>
2015-12-07 16:49:58 +0100

    Number generator coded

--- app/src/main/java/com/example/david/guessthenumber/MyActivity.java
+++ app/src/main/java/com/example/david/guessthenumber/MyActivity.java
@@ -2,6 +2,7 @@ package com.example.david.guessthenumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,7 +12,8 @@ public class MyActivity extends AppCompatActivity {
     private EditText number;
     private Button newgame,ok;
     private TextView result;
-
+    private int TheNumber;
+    private int Probes=3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +24,32 @@ public class MyActivity extends AppCompatActivity {
         newgame=(Button)findViewById(R.id.BTNewGame);
         ok=(Button)findViewById(R.id.BTOK);
         result=(TextView)findViewById(R.id.txtResult);
+        TheNumber=GenerateNumber();
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int guess = Integer.parseInt(number.getText().toString());
+                if (guess == TheNumber)
+                {
+                    result.setText("Gratulálok! Kitaláltad a számot!");
+                }
+                else
+                {
+                    Probes--;
+                }
+                if(Probes==0)
+                {
+                    ok.setEnabled(false);
+                    result.setText("Nincs lehetőséged tovább találgatni!");
+                }
+            }
+        });
+    }
+
+    private int GenerateNumber()
+    {
+        return (int)Math.random()*10+1;
     }
 }
